5c09a414205ffb09c500a1a4a0aa9942ccadf029
Mark Van Puymbrouck <markvanpuymbrouck@hotmail.com>
2018-04-19 13:57:45 -0700

    Ya Changes
    Project 2 Changes

--- MarksApp/app/src/main/java/com/example/mark/marksapp/MainActivity.java
+++ MarksApp/app/src/main/java/com/example/mark/marksapp/MainActivity.java
@@ -9,8 +9,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Random rand = new Random();
-    //int Die = rand.nextInt(6);
+    //String[] Sayings = {"Hi", "Bye", "Why are you still Here", "Please Leave"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +17,13 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClick(View view) {
 
-        Random rand = new Random();
-        int Die = rand.nextInt(6) + 1;
+        String[] Sayings = {"1)Hi", "2)Bye", "3)Why are you still Here", "4)Please Leave", "5)Just Stop", "6)Not again"};
+        Random r = new Random();
+        String saying = Sayings[r.nextInt(Sayings.length)];
+        //int Die = rand.nextInt(limit 6) +1;
 
         TextView counter = (TextView) findViewById(R.id.Number);
-        counter.setText(String.valueOf(Die));
+        counter.setText(String.valueOf(saying));
 
         TextView tv = (TextView) findViewById(R.id.Roll);
         tv.setText("Roll Again");

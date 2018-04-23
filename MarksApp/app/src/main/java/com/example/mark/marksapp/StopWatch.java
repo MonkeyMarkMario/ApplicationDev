d49ab0a95b3d0a0795a3ecf3cdfc8a5bbdcf91e2
Mark Van Puymbrouck <38263380+MonkeyMarkMario@users.noreply.github.com>
2018-04-23 14:02:26 -0700

    The Start of Pro3
    The start of Project 3 and my insanity.

--- MarksApp/app/src/main/java/com/example/mark/marksapp/StopWatch.java
+++ MarksApp/app/src/main/java/com/example/mark/marksapp/StopWatch.java
@@ -1,13 +1,13 @@
 package com.example.mark.marksapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class StopWatch extends AppCompatActivity {
 
     //String[] Sayings = {"Hi", "Bye", "Why are you still Here", "Please Leave"};
     @Override
@@ -20,16 +20,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClick(View view) {
 
-        String[] Sayings = {"1)Hi", "2)Bye", "3)Why are you still Here", "4)Please Leave", "5)Just Stop", "6)Not again"};
-        Random r = new Random();
-        String saying = Sayings[r.nextInt(Sayings.length)];
-        //int Die = rand.nextInt(limit 6) +1;
-
-        TextView counter = (TextView) findViewById(R.id.Number);
-        counter.setText(String.valueOf(saying));
-
-        TextView tv = (TextView) findViewById(R.id.Roll);
-        tv.setText("Roll Again");
     }
 
     @Override

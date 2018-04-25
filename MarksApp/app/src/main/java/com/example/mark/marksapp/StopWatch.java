286ec903eeaddaed00d7089f5f0dbb4d50ad19c7
Mark Van Puymbrouck <38263380+MonkeyMarkMario@users.noreply.github.com>
2018-04-25 15:01:45 -0700

    Pro3
    Project 3 Finished.

--- MarksApp/app/src/main/java/com/example/mark/marksapp/StopWatch.java
+++ MarksApp/app/src/main/java/com/example/mark/marksapp/StopWatch.java
@@ -1,48 +1,83 @@
 package com.example.mark.marksapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.util.Random;
+import java.util.Locale;
 
-public class StopWatch extends AppCompatActivity {
+public class stopwatch extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //if savedInstanceState !=null {
-
-        //}
+        setContentView(R.layout.stopwatch);
+        runTimer();
+        if (savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean ("running");
+            wasRunning = savedInstanceState.getBoolean ("wasRunning");
+        }
     }
-    public void startClick (View view) {
-
-    }
-
-    public void stopClick (View view){
-
-    }
-
-    public void resetClick (View view){
-
-    }
-
-    /*private void updateTimer (float time){
-            secs = (long) (time / 1000);
-            mins = (long)((time/1000)/60);
-            hrs = (long)(((time/1000)/60)/60);
-
-            secs = secs %60;
-            seconds = String.valueOf(secs);
-
-    }*/
 
     @Override
     public void onSaveInstanceState (Bundle savedInstanceState) {
-
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean ("running", running);
 
         super.onSaveInstanceState(savedInstanceState);
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        running = false;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if (wasRunning){
+            running = true;
+        }
+    }
+
+    public void startClick (View view) {
+        running = true;
+    }
+
+    public void stopClick (View view){
+        running = false;
+    }
+
+    public void resetClick (View view){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.Timer);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
 }
